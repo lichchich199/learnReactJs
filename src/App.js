0c76d9ebb3378f17fe6c4809bb65778e6c7c275d
hieutx <xhieu66@gmail.com>
2023-05-08 22:47:03 +0700

    router

--- src/App.js
+++ src/App.js
@@ -1,32 +1,15 @@
 import './App.css';
-import { BrowserRouter, Routes, Route, Link , Switch} from 'react-router-dom';
+import { BrowserRouter, Routes, Route} from 'react-router-dom';
 import Login from './views/Login';
 
 const Home = () => <h1>Home</h1>;
-const About = () => <h1>About</h1>;
 function App() {
   return (
-    // <BrowserRouter>
-    //     <Routes>
-    //       <Route path="/" element={<Login/>}/>
-    //     </Routes>
-    // </BrowserRouter>
-    // <>Hi</>
-    
     <BrowserRouter>
-    <Link to="/">Home</Link>{' '}
-      <Link to={{pathname: '/about'}}>About</Link>{' '}
-      <Link to="/contact">Contact</Link>
-        <Route path="/" component={Home} />
-        <Route path="/about" component={About} />
-        <Route
-          path="/contact"
-          render={() => <h1>Contact Us</h1>} />
-        <Route path="/blog" children={({match}) => (
-          <li className={match ? 'active' : ''}>
-            <Link to="/blog">Blog</Link>
-          </li>)} />
-        <Route render={() => <h1>Page not found</h1>} />
+        <Routes>
+          <Route path="/" exact element={<Login/>}/>
+          <Route path="/Home" element={<Home/>}/>
+        </Routes>
     </BrowserRouter>
   )
 }
